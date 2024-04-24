655002c6218433eaca1e3daab6e2c7b93f2652f4
Hansen Hansen <hansen@LaptopvonHansen.fritz.box>
2024-04-24 11:54:45 +0200

    frontend build new

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+
+const Note = require('./models/note')
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
 
 app.use(cors())
 app.use(express.json())
@@ -33,57 +50,113 @@ let notes = [
     return maxId + 1
   }
 
+
+
   app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
   
-  app.get('/api/notes',(request, response) => {
-    response.json(notes)
+
+  app.get('/api/notes', (request, response) => {
+
+    Note.find({}).then(notes => {
+      response.json(notes)
+    })
   })
 
 
-  app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note =notes.find(note => note.id === id)
-    if(note)
-    {
-      response.json(note)
-    }
-    else
-    {
-      response.statusMessage = "Data tidak ketemu"
-      response.status(404).end()
-    }
+  app.get('/api/notes/:id', (request, response, next) => {
+  
+    console.log('Note id:',request.params.id)
+
+    Note.findById(request.params.id)
+        .then(note => {
+          if(note)
+          {
+            response.json(note)
+          }
+          else
+          {
+            response.statusMessage = "ID does not exist!"
+            response.status(404).end()
+          }
+        })
+        .catch(error => next(error))
+
   })
 
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id != id)
-    response.status(204).end()
+  app.delete('/api/notes/:id', (request, response, next) => {
+    const id = request.params.id
+    console.log('Note id to delete:',id)
+    Note.findByIdAndDelete(id)
+        .then(result => {
+          console.log(result)
+          response.status(204).end()
+        })
+        .catch(error=>next(error))
   })
+
+
+  // app.post('/api/notes', (request, response) => {
+  //   const body = request.body
+
+  //   if(!request.body)
+  //   {
+  //     return response.status(404).json({
+  //       error:'Content missing!'
+  //     })
+  //   }
+
+  //   const note = {
+  //     content: body.content,
+  //     important: Boolean(body.important) || false,
+  //     id: generateId(),
+  //   }
+  
+  //   notes = notes.concat(note)
+    
+  //   response.json(notes)
+   
+  // })
 
   app.post('/api/notes', (request, response) => {
     const body = request.body
-
-    if(!request.body)
-    {
-      return response.status(404).json({
-        error:'Content missing!'
-      })
+  
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
+  
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+    })
+  
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
+  })
 
+  app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+  
     const note = {
       content: body.content,
-      important: Boolean(body.important) || false,
-      id: generateId(),
+      important: body.important,
     }
   
-    notes = notes.concat(note)
-    
-    response.json(notes)
-   
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
   })
+
+  //Unknown Endpoint Middleware
+  app.use(unknownEndpoint)
   
+  //Cast Error Middleware
+  app.use(errorHandler)
+
   const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
---
+++ models/note.js
@@ -0,0 +1,40 @@
+
+
+// const url =
+//   `mongodb+srv://aveirohansen101:${password}@fullstack.aksmlmp.mongodb.net/?retryWrites=true&w=majority&appName=fullstack`
+//CtZVQBqRrBM5R06w
+
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+
+const url = process.env.MONGODB_URI
+
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+
+module.exports = mongoose.model('Note', noteSchema)
